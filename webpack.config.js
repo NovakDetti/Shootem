eccec20d3d8aa5de76c9380f54c855348ceb7c7a
NovakDetti <54432325+NovakDetti@users.noreply.github.com>
2020-08-23 17:05:38 +0200

    Add webpack and live-server

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    entry: './launch.js',
+    output: {
+        filename: './build/main.js'
+    },
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.ts', '.js']
+    },
+    module: {
+    }
+};
